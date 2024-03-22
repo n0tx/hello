06a9b724481fc58dc96b67872daa76f7d0a70731
n0tx <rcandra91@msn.com>
2024-03-23 03:03:47 +0700

    print hello world using commandline runner

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -2,10 +2,16 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class HelloApplication {
-
+public class HelloApplication implements CommandLineRunner {
+  
+   @Override
+    public void run(String args[]) throws Exception { 
+        // Print statement when method is called 
+        System.out.println("Hello world!, using command line runner..."); 
+    } 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
